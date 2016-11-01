f933a85d050a80a81f397a4c765b819dadcd7637
kenji tomita <destinys.lotus.1108mn@gmail.com>
2016-11-01 09:00:16 +0900

    add VRTextureButton

--- AS/app/src/main/java/com/github/tommykw/as/VRTextureButton.kt
+++ AS/app/src/main/java/com/github/tommykw/as/VRTextureButton.kt
@@ -1,8 +1,12 @@
 package com.github.tommykw.`as`
 
+import jmini3d.Blending
 import jmini3d.Object3d
+import jmini3d.Texture
 import jmini3d.Vector3
 import jmini3d.geometry.VariableGeometry
+import jmini3d.material.Material
+import java.util.*
 import kotlin.properties.Delegates
 
 class VRTextureButton {
@@ -12,11 +16,47 @@ class VRTextureButton {
     private var vectorB = Vector3()
     private var vectorC = Vector3()
 
-    var geometry: VariableGeometry by Delegates.notNull()
-    var object3d: Object3d by Delegates.notNull()
+    private var geometry: VariableGeometry by Delegates.notNull()
+    private var object3d: Object3d by Delegates.notNull()
+    private var targetScale: Float = 1f
+    private var lookingToMe = false
+    private var timeLookingToMe = false
+    private var clickListener: VRClickListener? = null
+
+    fun VRTextureButton(width: Float,
+                        height: Float,
+                        texture: String,
+                        clickListener: VRClickListener) {
+        this.width = width
+        this.height = height
+        this.clickListener = clickListener
+
+        geometry = VariableGeometry(4, 2).apply {
+            addQuad(
+                Vector3(-width / 2, height / 2, 0f),
+                Vector3(width / 2, height / 2, 0f),
+                Vector3(-width / 2, -height / 2, 0f),
+                Vector3(width / 2, -height / 2, 0f)
+            )
+        }
+
+        val material = Material(Texture(texture)).apply {
+            setBlending(Blending.NormalBlending)
+        }
+
+        object3d = Object3d(geometry, material)
+    }
+
+    fun setPosition(x: Float, y: Float, z: Float) {
+        object3d.setPosition(x, y, z)
+    }
 
     companion object {
         const val TIME_CLICK = 1000
         const val SCALE_ANIMATION_SPEED = 3f
+
+        interface VRClickListener {
+            fun onClicked()
+        }
     }
 }
